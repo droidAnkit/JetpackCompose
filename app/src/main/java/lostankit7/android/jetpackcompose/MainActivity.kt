ff7c58a2819b261047fe03d6677338e48042d2c4
Ankit <ankit.kumar1@tokopedia.com>
2022-04-23 22:52:24 +0530

    rows and col

--- app/src/main/java/lostankit7/android/jetpackcompose/MainActivity.kt
+++ app/src/main/java/lostankit7/android/jetpackcompose/MainActivity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,12 +22,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             Row(
                 modifier = Modifier
-                    .width(10.dp)
-                    .background(Color.Blue)
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .background(Color.Blue),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(text = "Learning")
-                Text(text = "Compose")
-                Text(text = "Yeh!!")
+                Text(text = "Learning", color = Color.White)
+                Column(
+                    modifier = Modifier.fillMaxHeight(),
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text(text = "It's", color = Color.White)
+                    Text(text = "actually", color = Color.White)
+                    Text(text = "nice", color = Color.White)
+                }
+                Text(text = "Compose", color = Color.White)
             }
         }
     }
