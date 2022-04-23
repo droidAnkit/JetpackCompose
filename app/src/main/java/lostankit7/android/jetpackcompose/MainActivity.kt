1ea5af6ffb098c313e4fd7432a124c968a5124c7
Ankit <ankit.kumar1@tokopedia.com>
2022-04-23 23:42:27 +0530

    modifiers

--- app/src/main/java/lostankit7/android/jetpackcompose/MainActivity.kt
+++ app/src/main/java/lostankit7/android/jetpackcompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,6 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import lostankit7.android.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -23,22 +27,30 @@ class MainActivity : ComponentActivity() {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(100.dp)
+                    .fillMaxHeight(fraction = 0.4f)
+                    //.offset(10.dp, 20.dp)
+                    .border(5.dp, color = Color.Black)
+                    .padding(5.dp)
+                    .border(3.dp, Color.Yellow)
+                    .padding(3.dp)
                     .background(Color.Blue),
                 horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.CenterVertically
+//                verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(text = "Learning", color = Color.White)
-                Column(
-                    modifier = Modifier.fillMaxHeight(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.Start
-                ) {
-                    Text(text = "It's", color = Color.White)
-                    Text(text = "actually", color = Color.White)
-                    Text(text = "nice", color = Color.White)
-                }
-                Text(text = "Compose", color = Color.White)
+                Text(text = "It's", color = Color.Red,
+                    modifier = Modifier
+                        .padding(3.dp)
+                        .background(Color.Yellow)
+                        .fillMaxWidth(.25f)
+                        .fillMaxHeight(.25f)
+                        .padding(1.dp)
+                        .align(Alignment.CenterVertically)
+                        .border(2.dp, Color.DarkGray),
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline,
+                )
+                Text(text = "actually", color = Color.Red)
+                Text(text = "nice", color = Color.White)
             }
         }
     }
