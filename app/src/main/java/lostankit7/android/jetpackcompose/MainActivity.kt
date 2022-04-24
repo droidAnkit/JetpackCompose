f72cb26f56a5ee481350a229251369373395228b
Ankit <ankit.kumar1@tokopedia.com>
2022-04-24 22:17:47 +0530

    state in compose

--- app/src/main/java/lostankit7/android/jetpackcompose/MainActivity.kt
+++ app/src/main/java/lostankit7/android/jetpackcompose/MainActivity.kt
@@ -12,6 +12,9 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,9 +24,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import lostankit7.android.jetpackcompose.ui.compose.ImageCard
-import lostankit7.android.jetpackcompose.ui.compose.RowColModifiers
-import lostankit7.android.jetpackcompose.ui.compose.TextStyling
+import lostankit7.android.jetpackcompose.ui.compose.*
 import lostankit7.android.jetpackcompose.ui.theme.JetpackComposeTheme
 import lostankit7.android.jetpackcompose.ui.utils.showToast
 
@@ -32,14 +33,17 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-                    .border(width = 2.dp, Color.Blue)
-                    .padding(7.dp)
-            ) {
-                TextStyling()
+            val color = remember { mutableStateOf(Color.Yellow) }
+            Column(Modifier.fillMaxSize()) {
+                ColorGeneratorBox(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    color.value = it
+                }
+                ColorBox(modifier = Modifier
+                    .weight(1f),
+                    color = color
+                )
             }
         }
     }
---
+++ app/src/main/java/lostankit7/android/jetpackcompose/ui/compose/State.kt
@@ -0,0 +1,44 @@
+package lostankit7.android.jetpackcompose.ui.compose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import kotlin.random.Random
+
+@Composable
+fun ColorGeneratorBox(
+    modifier: Modifier = Modifier,
+    generateColor: (Color) -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Red)
+            .clickable {
+                generateColor.invoke(
+                    Color(
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        1f
+                    )
+                )
+            }
+    )
+}
+
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    color: State<Color>,
+) {
+    Box(modifier = modifier
+        .fillMaxSize()
+        .background(color.value)
+    )
+}
