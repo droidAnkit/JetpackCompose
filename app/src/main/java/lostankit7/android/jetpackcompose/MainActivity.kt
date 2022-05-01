9d05cad01e8d7532b4b9847801a52590e5fa4b8b
Ankit <ankit.kumar1@tokopedia.com>
2022-05-01 10:45:23 +0530

    custom progress bar

--- app/src/main/java/lostankit7/android/jetpackcompose/MainActivity.kt
+++ app/src/main/java/lostankit7/android/jetpackcompose/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.ExperimentalMaterialApi
@@ -15,10 +17,16 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import kotlinx.coroutines.delay
@@ -34,34 +42,61 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var sizeOfBox by remember { mutableStateOf(200.dp) }
-            val size by animateDpAsState(
-                targetValue = sizeOfBox,
-                tween(1000)
-//                spring(),
-            )
-
-            val infiniteTransaction = rememberInfiniteTransition()
-            val color by infiniteTransaction.animateColor(
-                initialValue = Color.Red,
-                targetValue = Color.Green,
-                animationSpec = infiniteRepeatable(
-                    tween(2000),
-                    RepeatMode.Reverse
-                )
-            )
-
-            Box(modifier = Modifier
-                .size(size)
-                .background(color),
-                contentAlignment = Alignment.Center
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
             ) {
-                Button(onClick = {
-                    sizeOfBox += 50.dp
-                }) {
-
-                }
+                CircularProgressBar(progress = 0.8f, number = 100)
             }
         }
+    }
+}
+
+@Composable
+fun CircularProgressBar(
+    progress: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animationDuration: Int = 1000,
+    animDelay: Int = 0,
+) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+
+    val currPercentage = animateFloatAsState(
+        targetValue = if (animationPlayed) progress else 0f,
+        animationSpec = tween(
+            durationMillis = animationDuration,
+            delayMillis = animDelay
+        )
+    )
+
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ) {
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                startAngle = -90f,
+                sweepAngle = 360 * currPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        Text(
+            text = (currPercentage.value * number).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
