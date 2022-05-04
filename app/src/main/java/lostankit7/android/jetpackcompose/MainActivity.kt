232211192c2eba01030df39ab0d53cb217e59d98
Ankit <ankit.kumar1@tokopedia.com>
2022-05-04 17:55:08 +0530

    bottom navigation with badges

--- app/src/main/java/lostankit7/android/jetpackcompose/MainActivity.kt
+++ app/src/main/java/lostankit7/android/jetpackcompose/MainActivity.kt
@@ -8,37 +8,148 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import lostankit7.android.jetpackcompose.ui.compose.Navigation
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
 @ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
 
-    private var i = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(
-                modifier = Modifier.fillMaxSize()
+            val navController = rememberNavController()
+            Scaffold(
+                bottomBar = {
+                    BottomNavigationBar(items = bottomNavItems,
+                        navController = navController,
+                        onItemClick = {
+                            navController.navigate(it.route)
+                        })
+                }
             ) {
-                Navigation()
+                Navigation(navController = navController)
             }
         }
     }
 }
+
+@Composable
+fun Navigation(
+    navController: NavHostController,
+) {
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen()
+        }
+        composable("chat") {
+            ChatScreen()
+        }
+        composable("settings") {
+            SettingsScreen()
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun BottomNavigationBar(
+    items: List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomNavItem) -> Unit,
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = Color.DarkGray,
+        elevation = 5.dp
+    ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                selectedContentColor = Color.Green,
+                unselectedContentColor = Color.Gray,
+                icon = {
+                    Column(
+                        horizontalAlignment = CenterHorizontally
+                    ) {
+                        if (item.badgeCount > 0) {
+                            BadgeBox(
+                                badgeContent = {
+                                    Text(text = item.badgeCount.toString())
+                                }
+                            ) {
+                                Icon(imageVector = item.icon, contentDescription = item.name)
+                            }
+                        } else {
+                            Icon(imageVector = item.icon, contentDescription = item.name)
+                        }
+                        if (selected) {
+                            Text(text = item.name, textAlign = TextAlign.Center, fontSize = 10.sp)
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Home Screen")
+    }
+}
+
+@Composable
+fun ChatScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Chat Screen")
+    }
+}
+
+@Composable
+fun SettingsScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Settings Screen")
+    }
+}
+
+private val bottomNavItems = listOf(
+    BottomNavItem("Home", "home", Icons.Default.Home),
+    BottomNavItem("Chat", "chat", Icons.Default.Notifications),
+    BottomNavItem("Settings", "settings", Icons.Default.Settings),
+)
