215e81b574bc9413cfd621c5f92759d8efa61c12
Ankit <ankit.kumar1@tokopedia.com>
2022-05-05 11:17:43 +0530

    permission in compose

--- app/src/main/java/lostankit7/android/jetpackcompose/ListItem.kt
+++
@@ -1,9 +0,0 @@
-package lostankit7.android.jetpackcompose
-
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-
-data class ListItem(
-    val title: String,
-    var isSelected: MutableState<Boolean> = mutableStateOf(false),
-)
--- app/src/main/java/lostankit7/android/jetpackcompose/MainActivity.kt
+++ app/src/main/java/lostankit7/android/jetpackcompose/MainActivity.kt
@@ -1,77 +1,82 @@
 package lostankit7.android.jetpackcompose
 
-import android.content.res.Resources
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.BottomCenter
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.TransformOrigin
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
-import androidx.compose.ui.input.nestedscroll.NestedScrollSource
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import lostankit7.android.jetpackcompose.ui.theme.JetpackComposeTheme
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionState
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
 @ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
 
+    @ExperimentalPermissionsApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                items(items.size) { i ->
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                items[i].isSelected.value = !items[i].isSelected.value
+            val permissionsState = rememberMultiplePermissionsState(permissions = listOf(
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.CAMERA,
+            ))
+
+            val lifeCycleOwner = LocalLifecycleOwner.current
+            DisposableEffect(key1 = lifeCycleOwner, effect = {
+                val observer = LifecycleEventObserver { _, event ->
+                    if(event == Lifecycle.Event.ON_RESUME) {
+                        permissionsState.launchMultiplePermissionRequest()
+                    }
+                }
+                lifeCycleOwner.lifecycle.addObserver(observer)
+
+                onDispose {
+                    lifeCycleOwner.lifecycle.removeObserver(observer)
+                }
+            })
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                permissionsState.permissions.forEach { permission ->
+                    when (permission.permission) {
+                        Manifest.permission.RECORD_AUDIO -> {
+                            when {
+                                permission.hasPermission -> {
+                                    Text(text = "Camera permission accepted")
+                                }
+                                permission.shouldShowRationale -> {
+                                    Text(text = "Camera permission is needed to access the camera")
+                                }
+                                permission.isPermanentlyDenied() -> {
+                                    Text(text = "Camera permission permanently denied, You can enable in app settings")
+                                }
                             }
-                            .padding(16.dp),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(text = items[i].title)
-                        if (items[i].isSelected.value) {
-                            Icon(
-                                imageVector = Icons.Default.Check,
-                                contentDescription = null,
-                                tint = Color.Green,
-                                modifier = Modifier.size(20.dp)
-                            )
+                        }
+                        Manifest.permission.CAMERA -> {
+                            when {
+                                permission.hasPermission -> {
+                                    Text(text = "Audio permission accepted")
+                                }
+                                permission.shouldShowRationale -> {
+                                    Text(text = "Audio permission is needed to play the music")
+                                }
+                                permission.isPermanentlyDenied() -> {
+                                    Text(text = "Audio permission permanently denied, You can enable in app settings")
+                                }
+                            }
                         }
                     }
                 }
@@ -79,8 +84,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private val items = (1..20).map {
-        ListItem("Item $it")
-    }
+    @ExperimentalPermissionsApi
+    private fun PermissionState.isPermanentlyDenied() =
+        !hasPermission && !shouldShowRationale
 
 }
